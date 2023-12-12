e26dcf4460c5b9f41a6c012df73208488712e952
William <68653853+will-i-amv@users.noreply.github.com>
2023-12-12 16:53:00 -0300

    Add print statements to show examples

--- Chapter_4/_06_Dunder_methods/matrices_with_scalars.py
+++ Chapter_4/_06_Dunder_methods/matrices_with_scalars.py
@@ -5,36 +5,41 @@ class Matrix:
     def __init__(self, rows):
         if len(set(len(row) for row in rows)) > 1:
             raise ValueError("All matrix rows must be the same length")
-
         self.rows = rows
 
     def __add__(self, other):
-        if len(self.rows) != len(other.rows) or len(self.rows[0]) != len(other.rows[0]):
+        if (
+            len(self.rows) != len(other.rows) or 
+            len(self.rows[0]) != len(other.rows[0])
+        ):
             raise ValueError("Matrix dimensions don't match")
-
         if isinstance(other, Matrix):
-            return Matrix(
+            return Matrix([
                 [
-                    [a + b for a, b in zip(a_row, b_row)]
-                    for a_row, b_row in zip(self.rows, other.rows)
+                    a + b 
+                    for a, b in zip(a_row, b_row)
                 ]
-            )
+                for a_row, b_row in zip(self.rows, other.rows)
+            ])
         elif isinstance(other, Number):
             return Matrix([[item + other for item in row] for row in self.rows])
         else:
             raise TypeError(f"Can't add {type(other)} to Matrix")
 
     def __sub__(self, other):
-        if len(self.rows) != len(other.rows) or len(self.rows[0]) != len(other.rows[0]):
+        if (
+            len(self.rows) != len(other.rows) or 
+            len(self.rows[0]) != len(other.rows[0])
+        ):
             raise ValueError("Matrix dimensions don't match")
-
         if isinstance(other, Matrix):
-            return Matrix(
+            return Matrix([
                 [
-                    [a - b for a, b in zip(a_row, b_row)]
-                    for a_row, b_row in zip(self.rows, other.rows)
+                    a - b 
+                    for a, b in zip(a_row, b_row)
                 ]
-            )
+                for a_row, b_row in zip(self.rows, other.rows)
+            ])
         elif isinstance(other, Number):
             return Matrix([[item - other for item in row] for row in self.rows])
         else:
@@ -44,19 +49,17 @@ class Matrix:
         if isinstance(other, Matrix):
             if len(self.rows[0]) != len(other.rows):
                 raise ValueError("Matrix dimensions don't match")
-
-            rows = [[0 for _ in other.rows[0]] for _ in self.rows]
-
+            rows = [
+                [0 for _ in other.rows[0]] 
+                for _ in self.rows
+            ]
             for i in range(len(self.rows)):
                 for j in range(len(other.rows[0])):
                     for k in range(len(other.rows)):
                         rows[i][j] += self.rows[i][k] * other.rows[k][j]
-
             return Matrix(rows)
-
         elif isinstance(other, Number):
             return Matrix([[item * other for item in row] for row in self.rows])
-
         else:
             raise TypeError(f"Can't multiply {type(other)} with Matrix")
 
@@ -74,34 +77,30 @@ class Matrix:
 
 
 if __name__ == "__main__":
-    m0 = Matrix(
-        [
-            [1, 0, 0],
-            [0, 1, 0],
-            [0, 0, 1],
-        ]
-    )
-    m1 = Matrix(
-        [
-            [1, 2, 3],
-            [4, 1, 4],
-            [5, 7, 9],
-        ]
-    )
+    m0 = Matrix([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+    ])
+    m1 = Matrix([
+        [1, 2, 3],
+        [4, 1, 4],
+        [5, 7, 9],
+    ])
     assert (m1 * m0).rows == m1.rows
-    m2 = Matrix(
-        [
-            [1, 2, 3],
-            [1, 4, 3],
-            [1, 0, 5],
-        ]
-    )
+    print(f'Matrix m1: \n{m1}\n')
+    m2 = Matrix([
+        [1, 2, 3],
+        [1, 4, 3],
+        [1, 0, 5],
+    ])
     assert (m2 * m0).rows == m2.rows
-
-    assert m1 * 2 == m1 + m1
-    assert 2 * m1 == m1 + m1
-    assert 3 * m1 == m1 + m1 + m1
-
-    assert m2 * 2 == m2 + m2
-    assert 2 * m2 == m2 + m2
-    assert 3 * m2 == m2 + m2 + m2
+    print(f'Matrix m2: \n{m2}\n')
+    m1_doubled = m1 * 2
+    print(f'Multiplication of m1 by 2: \n{m1_doubled}\n')
+    assert m1_doubled == m1 + m1
+    assert m1_doubled == 2 * m1
+    m2_tripled = m2 * 3
+    print(f'Multiplication of m2 by 3: \n{m2_tripled}\n')
+    assert m2_tripled ==  m2 + m2 + m2
+    assert m2_tripled == 3 * m2
