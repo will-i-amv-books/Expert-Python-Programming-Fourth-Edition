9ec5b8dad5921cdcfd1543a1b62cb3b8ba42868a
William <68653853+will-i-amv@users.noreply.github.com>
2023-12-19 16:30:55 -0300

    Add examples of a simple FOREX fetching app with different threading implememtations.

--- Chapter_6/_02_example_threaded_application/synchronous.py
+++ Chapter_6/_02_example_threaded_application/synchronous.py
@@ -10,11 +10,11 @@ def fetch_rates(base):
     response = requests.get(f"https://api.vatcomply.com/rates?base={base}")
     response.raise_for_status()
     rates = response.json()["rates"]
-
-    # note: same currency exchanges to itself 1:1
-    rates[base] = 1.0
-
-    rates_line = ", ".join([f"{rates[symbol]:7.03} {symbol}" for symbol in SYMBOLS])
+    rates[base] = 1.0  # same currency exchanges to itself 1:1
+    rates_line = ", ".join([
+        f"{rates[symbol]:7.03} {symbol}"
+        for symbol in SYMBOLS
+    ])
     print(f"1 {base} = {rates_line}")
 
 
@@ -27,6 +27,5 @@ if __name__ == "__main__":
     started = time.time()
     main()
     elapsed = time.time() - started
-
     print()
     print("time elapsed: {:.2f}s".format(elapsed))
--- Chapter_6/_05_Using_2way_queues/two_way_queues.py
+++ Chapter_6/_05_Using_2way_queues/two_way_queues.py
@@ -7,32 +7,34 @@ communication method.
 import time
 from queue import Queue, Empty
 from threading import Thread
+from typing import Dict, Tuple
 
 import requests
 
 
 SYMBOLS = ("USD", "EUR", "PLN", "NOK", "CZK")
 BASES = ("USD", "EUR", "PLN", "NOK", "CZK")
-
 THREAD_POOL_SIZE = 4
+Rates = Dict[str, float]
 
 
-def fetch_rates(base):
+def fetch_rates(base: str) -> Tuple[str, Rates]:
     response = requests.get(f"https://api.vatcomply.com/rates?base={base}")
-
     response.raise_for_status()
     rates = response.json()["rates"]
-    # note: same currency exchanges to itself 1:1
-    rates[base] = 1.0
+    rates[base] = 1.0  # same currency exchanges to itself 1:1
     return base, rates
 
 
-def present_result(base, rates):
-    rates_line = ", ".join([f"{rates[symbol]:7.03} {symbol}" for symbol in SYMBOLS])
+def present_result(base: str, rates: Rates) -> None:
+    rates_line = ", ".join([
+        f"{rates[symbol]:7.03} {symbol}"
+        for symbol in SYMBOLS
+    ])
     print(f"1 {base} = {rates_line}")
 
 
-def worker(work_queue, results_queue):
+def worker(work_queue: Queue, results_queue: Queue) -> None:
     while not work_queue.empty():
         try:
             item = work_queue.get_nowait()
@@ -46,23 +48,17 @@ def worker(work_queue, results_queue):
 def main():
     work_queue = Queue()
     results_queue = Queue()
-
     for base in BASES:
         work_queue.put(base)
-
     threads = [
         Thread(target=worker, args=(work_queue, results_queue))
         for _ in range(THREAD_POOL_SIZE)
     ]
-
     for thread in threads:
         thread.start()
-
     work_queue.join()
-
     while threads:
         threads.pop().join()
-
     while not results_queue.empty():
         present_result(*results_queue.get())
 
@@ -71,6 +67,5 @@ if __name__ == "__main__":
     started = time.time()
     main()
     elapsed = time.time() - started
-
     print()
     print("time elapsed: {:.2f}s".format(elapsed))
