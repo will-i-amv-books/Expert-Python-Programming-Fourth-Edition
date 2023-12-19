d17f48223afa95ac06a66cdf096c71578e295656
William <68653853+will-i-amv@users.noreply.github.com>
2023-12-19 17:30:48 -0300

    Add examples of exception handling and throttling to the summary

--- Chapter_6/_06_errors_in_threads/error_handling.py
+++ Chapter_6/_06_errors_in_threads/error_handling.py
@@ -8,23 +8,22 @@ import random
 import time
 from queue import Queue, Empty
 from threading import Thread
+from typing import Dict, Tuple
 
 import requests
 
 
 SYMBOLS = ("USD", "EUR", "PLN", "NOK", "CZK")
 BASES = ("USD", "EUR", "PLN", "NOK", "CZK")
-
 THREAD_POOL_SIZE = 4
+Rates = Dict[str, float]
 
 
-def fetch_rates(base):
+def fetch_rates(base: str) -> Tuple[str, Rates]:
     response = requests.get(f"https://api.vatcomply.com/rates?base={base}")
-
     if random.randint(0, 5) < 1:
         # simulate error by overriding status code
         response.status_code = 500
-
     response.raise_for_status()
     rates = response.json()["rates"]
     # note: same currency exchanges to itself 1:1
@@ -32,12 +31,15 @@ def fetch_rates(base):
     return base, rates
 
 
-def present_result(base, rates):
-    rates_line = ", ".join([f"{rates[symbol]:7.03} {symbol}" for symbol in SYMBOLS])
+def present_result(base: str, rates: Rates) -> None:
+    rates_line = ", ".join([
+        f"{rates[symbol]:7.03} {symbol}" 
+        for symbol in SYMBOLS
+    ])
     print(f"1 {base} = {rates_line}")
 
 
-def worker(work_queue, results_queue):
+def worker(work_queue: Queue, results_queue: Queue) -> None:
     while not work_queue.empty():
         try:
             item = work_queue.get_nowait()
@@ -56,28 +58,21 @@ def worker(work_queue, results_queue):
 def main():
     work_queue = Queue()
     results_queue = Queue()
-
     for base in BASES:
         work_queue.put(base)
-
     threads = [
         Thread(target=worker, args=(work_queue, results_queue))
         for _ in range(THREAD_POOL_SIZE)
     ]
-
     for thread in threads:
         thread.start()
-
     work_queue.join()
-
     while threads:
         threads.pop().join()
-
     while not results_queue.empty():
         result = results_queue.get()
         if isinstance(result, Exception):
             raise result
-
         present_result(*result)
 
 
@@ -85,6 +80,6 @@ if __name__ == "__main__":
     started = time.time()
     main()
     elapsed = time.time() - started
-
     print()
     print("time elapsed: {:.2f}s".format(elapsed))
+    
